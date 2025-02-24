52dfccee05ad3f120a403a8822baca1fd7162292
Phạm Quang Vũ <phamquangvu2308@gmail.com>
2025-02-24 09:16:07 +0700

    add ProductModel, FeedbackModel, CategoryModel, OrderModel, DiscountModel

---
+++ src/models/Feedback.Model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const feedbackSchema = new mongoose.Schema(
+  {
+    product_id: {type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true,},
+    variant_id: { type: String, required: false },
+    order_id: {type: mongoose.Schema.Types.ObjectId, ref: "Order", required: true,},
+    user_id: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true,},
+    content: { type: String, required: true },
+    feedback_media: {
+        images: [{ type: String }], 
+        videos: [{ type: String }], 
+    },
+    rating: { type: Number, required: true },
+    replied_by_admin: { type: String },
+  },
+  {
+    timestamps: true,
+    collection: "Feedback",
+  }
+);
+
+const Feedback = mongoose.model("Feedback", feedbackSchema);
+module.exports = Feedback;
---
+++ src/models/Product.Model.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const variantSchema = new mongoose.Schema(
+  {
+    variant_color: {type: String},
+    variant_size: {type: String},
+    variant_img: {
+        image_main: { type: String, required: true }, // Ảnh chính
+        image_subs: [{ type: String }] // Mảng chứa ảnh phụ
+    },
+    variant_price: { type: Number, required: true, default: 0 },
+    variant_countInStock: { type: Number, required: true, default: 0},
+    variant_percent_discount: {type: Number, default: 0}
+  }
+);
+  
+const productSchema = new mongoose.Schema(
+  {
+    product_title: { type: String, required: true },
+    product_category: { type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true },
+    product_brand: { type: String, required: true },
+    product_description: { type: String, required: true },
+    product_display: { type: Boolean, required: true, default: true },
+    product_famous: { type: Boolean, required: true, default: false },
+    product_rate: { type: Number, default: 0 },
+    product_feedback: [{ type: mongoose.Schema.Types.ObjectId, ref: "Feedback" }],
+    product_selled: { type: Number, required: false },
+    product_percent_discount: { type: Number },
+    product_price: { type: Number, required: true, default: 0},
+    product_countInStock: { type: Number, required: true, default: 0 },
+    variants: [variantSchema],
+  },
+  {
+    timestamps: true,
+    collection: "Product"
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
