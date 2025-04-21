ff27fc2c816eae8f010fb2d2c5960b8a90bb6b77
Duong Anh Vu <22521688@gm.uit.edu.vn>
2025-04-21 15:37:52 +0700

    test pipeline lan cuoi thu 7

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -25,7 +25,7 @@ function router(app) {
   app.use("/chat", chatbotRouter);
   app.use("/notification", notificationRouter);
   app.get("/", (req, res) => {
-    res.send("Hello WTM Sport Ecommerce Service!DAV ! an nhong ha se o");
+    res.send("Hello WTM Sport Ecommerce Service!DAV ! an nhong ha se o ôi! ");
   });
 }
 
