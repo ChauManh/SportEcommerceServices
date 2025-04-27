74cb14ebacb951ae5f9ebf604a48c70374a3b29a
Duong Anh Vu <22521688@gm.uit.edu.vn>
2025-04-27 17:40:57 +0700

    fix getProduct, train chatbot lan 2

--- src/data/model-chatbot/training.py
+++ src/data/model-chatbot/training.py
@@ -6,8 +6,12 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 response = openai.FineTuningJob.create(
-    training_file="file-5zEbE8vnuqWT7ojdwKsjbR", #Sau khi chạy upload.py sẽ có file_id, truyền id vào đây
-    model = "gpt-3.5-turbo",
+    training_file="file-P9eL4DWd9HZptmy5Jj4hpB", #Sau khi chạy upload.py sẽ có file_id, truyền id vào đây
+    model = "ft:gpt-3.5-turbo-0125:personal::BQCTOs0k",
+    hyperparameters={
+            "n_epochs": 4,
+        },
+    suffix="my-finetuned-model-v2" 
 )
 
 print(response)
